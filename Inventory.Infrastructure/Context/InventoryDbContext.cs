4e2cf47bdcbde146490ebf7ab3f8479a3aad03f3
JAVIERMAY <javiermay@iadb.org>
2023-02-28 17:53:55 -0500

    Implementing automatic modification to auditable fields

--- Inventory.Infrastructure/Context/InventoryDbContext.cs
+++ Inventory.Infrastructure/Context/InventoryDbContext.cs
@@ -15,5 +15,32 @@ namespace Inventory.Infrastructure.Context
         {
         }
         public DbSet<Item> Items { get; set; }
+
+        public override int SaveChanges()
+        {
+
+            var tracker = ChangeTracker;
+            foreach (var entry in tracker.Entries())
+            {
+                if (entry.Entity is FullAuditModel)
+                {
+                    var referenceEntity = entry.Entity as FullAuditModel;
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            referenceEntity.CreatedDate = DateTime.Now;
+                            break;
+                        case EntityState.Deleted:
+                        case EntityState.Modified:
+                            referenceEntity.LastModifiedDate = DateTime.Now;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            return base.SaveChanges();
+    }
+
     }
 }
